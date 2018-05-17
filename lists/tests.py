4baa8847ce1c7c0646107c073421241e996e159e
Jboy Flaga <flaga.jeremiah@gmail.com>
2018-05-18 00:59:15 +0800

    Used Django Test Client in unit test - it has built-in ways of checking what templates are used

--- lists/tests.py
+++ lists/tests.py
@@ -11,9 +11,6 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
